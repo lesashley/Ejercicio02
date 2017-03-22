e7459adc248e93623cdee3200076109aea24b010
lesashley <lesashley56@gmail.com>
2017-03-22 11:18:16 -0500

    Identación

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,15 @@
-//alert("hola");
-
 var datos = [ { nombre : "Arabela", apellido : "Rojas", rol : "T.A", cumpleanios : "23 de enero" },
- { nombre : "Michelle", apellido : "Seguil", rol : "T.A", cumpleanios : "25 de noviembre" },
- { nombre : "Meche", apellido : "Zubieta", rol : "T.A", cumpleanios : "02 de marzo" },
- { nombre : "Papu", apellido : "Rivariola", rol : "Psicóloga", cumpleanios : "10 de octubre" },
- { nombre : "Gian", apellido : "Corzo", rol : "Profesor", cumpleanios : "23 de enero" } ];
+              { nombre : "Michelle", apellido : "Seguil", rol : "T.A", cumpleanios : "25 de noviembre" },
+              { nombre : "Meche", apellido : "Zubieta", rol : "T.A", cumpleanios : "02 de marzo" },
+              { nombre : "Papu", apellido : "Rivariola", rol : "Psicóloga", cumpleanios : "10 de octubre" },
+              { nombre : "Gian", apellido : "Corzo", rol : "Profesor", cumpleanios : "23 de enero" } ];
 
-var show = document.getElementById("area");
- //function mostrarPropiedades(objeto) {
-  var resultado = "";
+var mostrar = document.getElementById("area");
+var resultado = "";
  datos.forEach(function(e){
    resultado += "<div><ul><li>Nombre: " + e.nombre + "</li>"+
                 "<li>Apellido: " + e.apellido + "</li>"+
                 "<li>Rol: " + e.rol + "</li>"+
                 "<li>Cumpleaños: " + e.cumpleanios + "</li></ul></div>";
  })
-show.innerHTML= resultado;
+mostrar.innerHTML= resultado;
